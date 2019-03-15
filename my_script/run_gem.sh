ee43a099db292b652c57dfaf71a0ebf099a2aee8
Andrew Zigerelli <andrewzigerelli@users.noreply.github.com>
2019-03-15 12:19:47 -0400

    small update
    Change-Id: I82f58b04a70ec27faffadf74bec027ffefc10a1e

--- my_script/run_gem.sh
+++ my_script/run_gem.sh
@@ -44,7 +44,7 @@ L3_ASSOC="8"
 USE_RUBY="TRUE" #SET TO TRUE TO USE RUBY
 
 #MAXINSTS=10000000000
-MAXINSTS=100000000
+MAXINSTS=1000000000
 
 #### DISK_IMAGE FOLLOW BY root=option since each 
 #### image has a different root partition
@@ -156,14 +156,14 @@ READFILE_NUMBER=0
 BENCH_READFILE="$(pwd)/readfiles/$BENCHMARK"
 BENCH_OPTIONS="--checkpoint-dir=$CKPT_DIR --script $BENCH_READFILE --kernel
 $KERNEL --disk-image $DISK_IMAGE --cpu-type=$CPU_TYPE
---restore-with-cpu=$CKPT_CPU_TYPE --maxinsts=$MAXINSTS --mem-size $MEM_SIZE $CACHE_OPTIONS $RCT_CFG -r 1"
+--restore-with-cpu=$CKPT_CPU_TYPE --maxinsts=$MAXINSTS --mem-size $MEM_SIZE $CACHE_OPTIONS -r 1"
 BENCH_OUT_DIR=$OUT_DIR/$BENCHMARK/
 #BENCH_DEBUG_FLAG=My_Flag
-BENCH_DEBUG_FLAG=Ruby,TLB #RCTStats
+BENCH_DEBUG_FLAG=RCTStats
 BENCH_DEBUG_FILE=my_trace.out.gz
 #buid full cmd, potentially unsafe if you screw up the builder variables
 FULL_CMD=$GEM_CMD" "--outdir=$BENCH_OUT_DIR" "--debug-flag=$BENCH_DEBUG_FLAG" "--debug-file=$BENCH_DEBUG_FILE" "$CFG" "$BENCH_OPTIONS" "--command-line" '"$CMD_LINE" root="$ROOT"'"
-FULL_CMD=$GEM_CMD" "--outdir=$BENCH_OUT_DIR" "--debug-start=3625891399000" "--debug-flag=$BENCH_DEBUG_FLAG" "--debug-file=$BENCH_DEBUG_FILE" "$CFG" "$BENCH_OPTIONS" "--command-line" '"$CMD_LINE" root="$ROOT"'"
+FULL_CMD=$GEM_CMD" "--outdir=$BENCH_OUT_DIR" "$CFG" "$BENCH_OPTIONS" "--command-line" '"$CMD_LINE" root="$ROOT"'"
 if [ "$1" = "--dry-run" ]; then
     echo $FULL_CMD
     exit
