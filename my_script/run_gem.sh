04a9e86c4494cad30d5800f65212018ba9384868
Andrew Zigerelli <andrewzigerelli@users.noreply.github.com>
2019-03-23 19:46:59 -0400

    updated my_script
    Change-Id: I9f74e6760ee330058709edeef51394384e34c373

--- my_script/run_gem.sh
+++ my_script/run_gem.sh
@@ -46,7 +46,7 @@ L3_ASSOC="8"
 USE_RUBY="TRUE" #SET TO TRUE TO USE RUBY
 
 #MAXINSTS=10000000000
-MAXINSTS=100000000
+MAXINSTS=10000000
 
 #### DISK_IMAGE FOLLOW BY root=option since each 
 #### image has a different root partition
@@ -161,10 +161,10 @@ READFILE_NUMBER=0
 BENCH_READFILE="$(pwd)/readfiles/$BENCHMARK"
 BENCH_OPTIONS="--checkpoint-dir=$CKPT_DIR --script $BENCH_READFILE --kernel
 $KERNEL --disk-image $DISK_IMAGE --cpu-type=$CPU_TYPE
---restore-with-cpu=$CKPT_CPU_TYPE --maxinsts=$MAXINSTS --mem-size $MEM_SIZE $CACHE_OPTIONS -r 1"
+--restore-with-cpu=$CKPT_CPU_TYPE --maxinsts=$MAXINSTS --mem-size $MEM_SIZE $CACHE_OPTIONS $RCT_CFG -r 1"
 BENCH_OUT_DIR=$OUT_DIR/$BENCHMARK/
 #BENCH_DEBUG_FLAG=My_Flag
-BENCH_DEBUG_FLAG=stallflag
+BENCH_DEBUG_FLAG=RCTStats
 BENCH_DEBUG_FILE=my_trace.out.gz
 #buid full cmd, potentially unsafe if you screw up the builder variables
 FULL_CMD=$GEM_CMD" "--outdir=$BENCH_OUT_DIR" "--debug-flags=$BENCH_DEBUG_FLAG" "$CFG" "$BENCH_OPTIONS" "--command-line" '"$CMD_LINE" root="$ROOT"'"
--- my_script/test_gem.sh
+++ my_script/test_gem.sh
@@ -4,7 +4,11 @@ cd $GEM5_PATH/my_script;
 BUILD_STRING=$(grep -E "^BUILD=" run_gem.sh)
 #extract just the ISA
 ISA=${BUILD_STRING:6}
+CORE_STRING=$(grep -E "^CORE_NUM=" run_gem.sh)
+CORE=${CORE_STRING:9}
+CORE="${CORE%\"}"
+CORE="${CORE#\"}"
 ./run_gem.sh --benchmark `ls readfiles | grep 502`;
-cd output/$ISA/ubuntu-16.img/vmlinux_4.19.0/502.gcc_r_readfile_0;
+cd output/$ISA/$CORE_NUM"_core"/ubuntu-16.img/vmlinux_4.19.0/502.gcc_r_readfile_0;
 gunzip -f my_trace.out.gz;
 vim my_trace.out;
