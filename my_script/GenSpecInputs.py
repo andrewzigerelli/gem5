8c122b93e83be1c605585a51ce7d2cf91a05896a
Andrew Zigerelli <andrewzigerelli@users.noreply.github.com>
2019-05-20 17:52:14 -0400

    changed to generate se inputs

--- my_script/GenSpecInputs.py
+++ my_script/GenSpecInputs.py
@@ -6,6 +6,12 @@ import re
 import pickle
 import time
 
+# readfile for FS mode is straightforward: just command to be run in gem5
+# for SE mode, it's 4 lines
+#  0:  BENCH_DIR
+#  1:  --cmd and --options lines
+#  2:  output file name
+#  3:  stderr file name
 
 def main():
     # get spec2017 path
@@ -13,14 +19,22 @@ def main():
         specPath = sys.argv[1]
     except:
         errMsg = "Need to give Spec2017 path as argument! "
-        errMsg += "e.g. \n python GenSpecInputs.py /opt/spec2017"
+        errMsg += "e.g. \n python GenSpecInputs.py /opt/spec2017 0001 se"
         raise ValueError(errMsg)
 
     try:
         runSuffix = sys.argv[2]
     except:
         errMsg = "Need to give run dir numbers as argument. "
-        errMsg += "e.g. \n python GenSpecInputs.py /opt/spec2017 0001"
+        errMsg += "e.g. \n python GenSpecInputs.py /opt/spec2017 0001 se"
+        raise ValueError(errMsg)
+
+    try:
+        mode = sys.argv[3]
+        mode = mode.rstrip()
+    except:
+        errMsg = "Need to give mode, either se or fs"
+        errMsg += "e.g. \n python GenSpecInputs.py /opt/spec2017 0001 se"
         raise ValueError(errMsg)
 
     # add benchspec/CPU
@@ -32,17 +46,16 @@ def main():
         # we can set specinvoke path
         specinvokePath = os.path.join(specPath, "bin")
         specinvokeBin = os.path.join(specinvokePath, "specinvoke")
-        print(benchesPath)
     else:
         raise ValueError("Spec2017 benchmark path not found")
 
     # get benchmarks
-    benches={}
+    benches = {}
     dirs = [
         d for d in os.listdir(benchesPath)
         if os.path.isdir(os.path.join(benchesPath, d))
     ]
-    benches = {d:os.path.join(benchesPath,d) for d in dirs}
+    benches = {d: os.path.join(benchesPath, d) for d in dirs}
 
     # make readfile directory and command file
     specFile = open("spec_cmds.txt", "w")
@@ -51,8 +64,15 @@ def main():
     except FileExistsError:
         pass
     cwd = os.getcwd()
-    readfileDir=os.path.join(cwd,"readfiles")
 
+    if mode == "fs":
+        readfileDir = os.path.join(cwd, "readfiles")
+    elif mode == "se":
+        readfileDir = os.path.join(cwd, "se_readfiles")
+    else:
+        errMsg = "Need to give mode, either se or fs"
+        errMsg += "e.g. \n python GenSpecInputs.py /opt/spec2017 0001 se"
+        raise ValueError(errMsg)
 
     # get and write commandline
     for bench, benchdir in benches.items():
@@ -82,29 +102,85 @@ def main():
         start = spec_out.find("# Starting run for copy")
         last = spec_out.find("specinvoke exit")
         substring = spec_out[start:last]
-        parseCmd(substring, readfileDir, bench)
+        parseCmd(substring, readfileDir, bench, mode)
         specFile.write(substring+"\n")
 
     # write to
     specFile.close()
 
-def parseCmd(cmd, readfileDir, bench):
+
+def parseCmd(cmd, readfileDir, bench, mode):
     lines = cmd.splitlines()
+
+    # regex for parsing se mode
+    cmdRegex = "^([^><]+?)?(< .+?)?(> .+?)?(2>> .+)$"
+    cmdRegex = re.compile(cmdRegex)
+
+    textRegex = "\s+.*"
+    textRegex = re.compile(textRegex)
+
     i = 0
     while len(lines) != 0:
         del lines[0]
-        command = lines[0] + ";" + lines[1] + ";m5 exit;"
-        del lines[0:2]
-        readfile=os.path.join(readfileDir,bench + "_readfile_" + str(i))
-        with open(readfile,"w") as f:
-            f.write(command)
-        i+=1
+        if mode == "fs":
+            command = lines[0] + ";" + lines[1] + ";m5 exit;"
+        else:
+            # we must create command by parsing text
+            runFolder = lines[0][3:]
+            binaryNamePosEnd = lines[1].find(' ')
+            binary = lines[1][:binaryNamePosEnd]
+            cmd = os.path.basename(os.path.normpath(binary))
+            rest = lines[1][binaryNamePosEnd + 1:]
 
+            command = runFolder + "\n" + "--cmd=" + cmd
+
+            # parse input options
+            result = cmdRegex.search(rest)
+            result = result.groups()
+
+            options = result[0]
+            if options:
+                options = options.rstrip()
+                command = command + ' --options="' + options + '"'
+
+            stdin = result[1]
+            if stdin:
+                stdin = stdin.rstrip()
+                text = textRegex.search(stdin)
+                text = text.group(0).lstrip()
+                command = command + " --input=" + text + "\n"
+            else:
+                command = command + "\n"
+
+            stdout = result[2]
+            if stdout:
+                stdout = stdout.rstrip()
+                text = textRegex.search(stdout)
+                text = text.group(0).lstrip()
+                command = command + text + "\n"
+            else:
+                command = command + "\n"
+
+            stderr = result[3]
+            if stderr:
+                stderr = stderr.rstrip()
+                text = textRegex.search(stderr)
+                text = text.group(0).lstrip()
+                command = command + text + "\n"
+            else:
+                command = command + "\n"
+            
+        del lines[0:2]
+        readfile = os.path.join(readfileDir, bench + "_readfile_" + str(i))
+        with open(readfile, "w") as f:
+            f.write(command)
+        i += 1
 
 
 def save_obj(obj, name):
     with open(name + '.pkl', 'wb') as f:
         pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
+
 
 def load_obj(name):
     with open(name + '.pkl', 'rb') as f:
