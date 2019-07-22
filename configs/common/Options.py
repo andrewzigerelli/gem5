acd2c81ddf7e45cab1cdd1c98f4af82b5cefcc7f
liu <tuinannan@163.com>
2019-07-22 15:30:33 -0400

    run on server
    Change-Id: Id9cbe38c04ca3713afdbb76ed4c3257d7fcdf811

--- configs/common/Options.py
+++ configs/common/Options.py
@@ -316,7 +316,7 @@ def addCommonOptions(parser):
     parser.add_option("--rct_size", type="int", default="100")
     parser.add_option("--num_ctrs", type="int", default="10")
     ## yanan
-    parser.add_option("--threshold", type="int", default="5")
+    parser.add_option("--threshold", type="int", default="10000")
 
 def addSEOptions(parser):
     # Benchmark options
